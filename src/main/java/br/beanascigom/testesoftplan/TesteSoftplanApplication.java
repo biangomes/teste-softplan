b520b1966af612be2d8c22947ad5cfc786de349d
Beatriz Gomes <beanascigom@gmail.com>
2026-04-13 21:16:06 -0300

    first commit

---
+++ src/main/java/br/beanascigom/testesoftplan/TesteSoftplanApplication.java
@@ -0,0 +1,13 @@
+package br.beanascigom.testesoftplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSoftplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteSoftplanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/beanascigom/testesoftplan/TesteSoftplanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.beanascigom.testesoftplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSoftplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
