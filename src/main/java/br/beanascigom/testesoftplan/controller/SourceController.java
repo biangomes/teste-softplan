00ac4fddb330b13c29b801d67c110e9b995d9b08
Beatriz Gomes <beanascigom@gmail.com>
2026-04-15 22:33:22 -0300

    chore: adjust result of source endpoint

--- src/main/java/br/beanascigom/testesoftplan/controller/SourceController.java
+++ src/main/java/br/beanascigom/testesoftplan/controller/SourceController.java
@@ -1,6 +1,5 @@
 package br.beanascigom.testesoftplan.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SourceController {
     @GetMapping(path = "/source")
-    public ResponseEntity<Object> source() {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<String> source() {
+        return ResponseEntity.ok("https://github.com/biangomes/teste-softplan");
     }
 }
